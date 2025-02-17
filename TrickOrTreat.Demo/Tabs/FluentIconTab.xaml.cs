9dbfb9d5c8e60ca5a303330228222ca4987067a4
Lee Dongju <dannyldj@gmdsoft.com>
2025-02-17 19:05:14 +0900

    Add project files.

---
+++ TrickOrTreat.Demo/Tabs/FluentIconTab.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace TrickOrTreat.Demo.Tabs;
+
+/// <summary>
+///     Interaction logic for FluentIconTab.xaml
+/// </summary>
+public partial class FluentIconTab : TabItem
+{
+    public FluentIconTab()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TrickOrTreat/Controls/FluentIcon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TrickOrTreat.Controls
+{
+    public partial class FluentIcon : TextBlock
+    {
+        public static readonly DependencyProperty SymbolProperty = DependencyProperty.Register(
+            nameof(Symbol), typeof(IconGlyph?), typeof(FluentIcon), new PropertyMetadata(null, SymbolPropertyChanged));
+
+        static FluentIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FluentIcon),
+                new FrameworkPropertyMetadata(typeof(FluentIcon)));
+        }
+
+        public IconGlyph? Symbol
+        {
+            get => (IconGlyph?)GetValue(SymbolProperty);
+            set => SetValue(SymbolProperty, value);
+        }
+
+        private static void SymbolPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is FluentIcon icon && e.NewValue != null)
+                icon.Text = Encoding.Unicode.GetString(BitConverter.GetBytes((ushort)e.NewValue));
+        }
+    }
+}
